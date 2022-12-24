234be315384b6aa4429c15c02fedcdcb2918f05b
hkshakib <humayunkibriashakib@gmail.com>
2022-12-24 23:58:00 +0600

    Collection APIView added

--- store/urls.py
+++ store/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('products/', views.ProductList.as_view()),
     path('products/<int:id>/', views.ProductDetails.as_view()),
 
-    path('collection/', views.collection_list),
-    path('collection/<int:pk>/', views.collection_details, name='collection-details'),
+    path('collection/', views.CollectionList.as_view()),
+    path('collection/<int:pk>/', views.CollectionDetails.as_view(), name='collection-details'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,5 @@
 from django.db.models.aggregates import Count
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -80,10 +79,23 @@ def product_details(request, id):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+class CollectionList(APIView):
+    def get(self, request):
+        queryset = Collection.objects.annotate(products_count=Count('products')).all()
+        serializer = CollectionSerializers(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CollectionSerializers(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
 @api_view(['GET', 'POST'])
 def collection_list(request):
     if request.method == 'GET':
-        queryset = Collection.objects.annotate(products_count=Count('Products')).all()
+        queryset = Collection.objects.annotate(products_count=Count('products')).all()
         serializer = CollectionSerializers(queryset, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
@@ -91,6 +103,31 @@ def collection_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class CollectionDetails(APIView):
+    def get(self, request, pk):
+        collection = get_object_or_404(Collection.objects.annotate(
+            products_count=Count('products')
+        ), pk=pk)
+        serializer = ProductSerializer(collection)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        serializer = CollectionSerializers(Collection, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request, pk):
+        collection = get_object_or_404(Collection.objects.annotate(
+            products_count=Count('products')
+        ), pk=pk)
+        if collection.products.count() > 0:
+            return Response({'error': "Can't Be Deleted , Because it's Include one or more products"},
+                            status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
