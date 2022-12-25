13322aa0272fb0c6ecdfa24cf9df84a5dc5bc985
hkshakib <humayunkibriashakib@gmail.com>
2022-12-25 12:07:21 +0600

    Added Generics Filtering

--- store/views.py
+++ store/views.py
@@ -1,25 +1,20 @@
 from django.db.models.aggregates import Count
-from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Collection, Product, Reviews
+from .filters import ProductFilter
 from .serializers import ProductSerializer, CollectionSerializers, ReviewSerializer
 
 
 # Create your views here.
 #
 class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        collection_id = self.request.query_params.get('collection_id')
-
-        if collection_id is not None:
-            queryset = queryset.filter(collection_id=collection_id)
-        return queryset
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
 
     def get_serializer_context(self):
         return {'request': self.request}
@@ -33,21 +28,6 @@ class ProductViewSet(ModelViewSet):
 class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('products')).all()
     serializer_class = CollectionSerializers
-
-
-class CollectionList(ListCreateAPIView):
-    queryset = Collection.objects.annotate(products_count=Count('products')).all()
-    serializer_class = CollectionSerializers
-
-    def delete(self, request, pk):
-        collection = get_object_or_404(Collection.objects.annotate(
-            products_count=Count('products')
-        ), pk=pk)
-        if collection.products.count() > 0:
-            return Response({'error': "Can't Be Deleted , Because it's Include one or more products"},
-                            status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class ReviewViewSet(ModelViewSet):
