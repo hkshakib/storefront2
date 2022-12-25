30c705c4c60e5aacee061f1add2fb94ea54c6458
hkshakib <humayunkibriashakib@gmail.com>
2022-12-25 12:23:39 +0600

    Added Searching, Ordering, Pagination into API

--- store/views.py
+++ store/views.py
@@ -1,20 +1,24 @@
 from django.db.models.aggregates import Count
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
-from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
+from .pagination import DefaultPagination
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Collection, Product, Reviews
 from .filters import ProductFilter
 from .serializers import ProductSerializer, CollectionSerializers, ReviewSerializer
 
 
-# Create your views here.
-#
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    # pagination_class = DefaultPagination
+    search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
 
     def get_serializer_context(self):
         return {'request': self.request}
